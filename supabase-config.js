5ce1a0e37f4f4c65f44a452611f446a9ebc797e8
MFRDS22 <mariofrancescodisauro@gmail.com>
2026-07-03 15:44:49 +0200

    Add Supabase configuration and client initialization

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// CONFIGURAZIONE CENTRALE SUPABASE
+const SUPABASE_URL = "sb_publishable_nvT77M2hMddasqrL1plQKw_r_G6n5VR";
+const SUPABASE_KEY = https://dutgtlznepbsmkuncgug.supabase.co/rest/v1/;
+
+// Inizializzazione del client Supabase globale
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
