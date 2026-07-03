c7bcf04af8d37303e6a31ded8c81f316e3300e3e
MFRDS22 <mariofrancescodisauro@gmail.com>
2026-07-03 16:06:45 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // CONFIGURAZIONE CENTRALE SUPABASE
-const SUPABASE_URL = "sb_publishable_nvT77M2hMddasqrL1plQKw_r_G6n5VR";
-const SUPABASE_KEY = https://dutgtlznepbsmkuncgug.supabase.co/rest/v1/;
+const SUPABASE_URL = "https://dutgtlznepbsmkuncgug.supabase.co/rest/v1";
+const SUPABASE_KEY = sb_publishable_nvT77M2hMddasqrL1plQKw_r_G6n5VR;
 
 // Inizializzazione del client Supabase globale
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
